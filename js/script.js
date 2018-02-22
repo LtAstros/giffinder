97bdfb3e401ccbe70381ff08e1de2fe43bb8352d
Costin Smilovici <costinsf11@gmail.com>
2018-02-22 01:22:21 +0000

    Got rid of $ errors

--- js/script.js
+++ js/script.js
@@ -1,24 +1,21 @@
+/* global $ */
 // *************************** YOUR CODE BELOW *******************************
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 
 $(document).ready(function(){
+  $("body").click(function(){
   
-  // WRITE A CLICK HANDLER HERE
-  /*
-    1. click handler function
-    2. get the typed input from the search input box
-    3. call the functions below!
-  */
-  
+  });  
+
+});
   function giphyURLWithSearchTerm(searchTerm) {
-      // write a function that will return a url for the giphy API with
-      // the searchTerm provided in the parameters
+    var newUrl = "https://api.giphy.com/v1/stickers/search?q="+searchTerm+"&api_key=dc6zaTOxFJmzC";
+    return newUrl;
   }
 
   function appendImageToGallery(srcURL) {
-      // write a function that will append an <img> to the div with class="gallery"
-      // using the URL provided in the parameters
+    $("body").append("<img src="+srcURL+">");
   }
 
   function callGiphyAPIWithSearchTerm(searchTerm) {
@@ -37,4 +34,4 @@ $(document).ready(function(){
       },
     }); 
   }
-});
+
